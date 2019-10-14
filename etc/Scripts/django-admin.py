937a34f4134c296c97c2773ac7174851cb7145d1
박성하 <rd1581@naver.com>
2019-10-14 13:59:56 +0900

    first-commit!

---
+++ etc/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\multicampus\desktop\final\s1p3151006\backend\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
