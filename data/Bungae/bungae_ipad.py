6dd5d4833725e82767ec8b0d14a256b3a60d15db
조신성 <nustar1005@naver.com>
2019-10-18 16:47:05 +0900

    S1P3151006-33 | 번개장터 : 데이터 전처리 완료 + 이미지 URL, 기본 링크 추가

--- data/Bungae/bungae_ipad.py
+++ data/Bungae/bungae_ipad.py
@@ -2,105 +2,165 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from datetime import datetime
+from datetime import timedelta
+import time
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome("../chromedriver.exe")
 driver.implicitly_wait(3)
 driver.get('https://m.bunjang.co.kr/')
 
 #번개장터 접속 후, 로그인
-login_number = driver.find_element_by_class_name("input-wrapper")
-login_number.find_element_by_name("phone").send_keys("01071193527")
-password = driver.find_element_by_name("password").send_keys("junggobi1014!")
-btn = driver.find_element_by_class_name("login-btn").click()
+login_number = driver.find_element_by_class_name("sc-cQFLBn")
+print(login_number)
+login_number.find_element_by_tag_name("input").send_keys("01071193527")
+password = driver.find_element_by_class_name("sc-gojNiO")
+password.find_element_by_tag_name("input").send_keys("junggobi1014!")
+btn = driver.find_element_by_class_name("sc-daURTG").click()
 
 #아이패드 검색
-search = driver.find_element_by_class_name("search-box")
-search.find_element_by_class_name("search-box-input").send_keys("아이패드")
-search.find_element_by_class_name("search-box-input").send_keys(Keys.RETURN)
+search = driver.find_element_by_class_name("sc-jKJlTe")
+search.find_element_by_class_name("sc-eNQAEJ").send_keys("아이패드")
+search.find_element_by_class_name("sc-eNQAEJ").send_keys(Keys.RETURN)
 
 #아이패드 리스트 찾기
-u_list = driver.find_element_by_class_name("category-product-list").find_elements_by_tag_name("img")
+u_list = driver.find_element_by_class_name("sc-gRnDUn").find_elements_by_tag_name("img")
 url_list = []
+img_url_list = []
 for i in u_list:
-    src = i.get_attribute("data-src")
-    src = src.split("https://seoul-p-studio.bunjang.net/product/")
-    src = src[1][0:9]
-    if src[8]=="_":
-        src = src[0:8]
-    url_list.append(src)
+    src_url = i.get_attribute("src")
+    src = src_url.split("https://seoul-p-studio.bunjang.net/product/")
+    if len(src)>=2:
+        src = src[1][0:9]
+        if src[8]=="_":
+            src = src[0:8]
+        url_list.append(src)
+        img_url_list.append(src_url)
+print(url_list)
+
+#오늘 날짜 확인
+today = datetime.today().strftime("%Y%m%d")
+
 
 #상세페이지 웹 크롤링
 cnt=0
 request_data = {'bungae': []}
 for data in url_list:
-    url = "https://m.bunjang.co.kr/products/"+data+"?ref=%EA%B2%80%EC%83%89%EA%B2%B0%EA%B3%BC&q=%EC%95%84%EC%9D%B4%ED%8C%A8%EB%93%9C"
+    url = "https://m.bunjang.co.kr/products/"+data+"?ref=검색결과&q=아이패드"
     driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+    try:
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-    sell = soup.select("#app > div.router-view > div > div.product-detail-wrapper > div:nth-child(2) > div:nth-child(1) > div.suggested-products-title")
-    if len(sell)==0:
-
-        title = soup.find_all("h2")
-        price = soup.find_all("h3")
-        status = soup.select("ul.product-summary__assistant-2nd>li.status")
-        exchange = soup.select("ul.product-summary__assistant-2nd>li.exchange")
-        shipping = soup.select("ul.product-summary__assistant-2nd>li.shipping")
-        location = soup.select("ul.product-summary__assistant-2nd>li.location")
-        description = soup.find_all("p")
-        id = data
-        title = title[0].text.strip()
-        if "매입" not in title:
-            cnt+=1
-            cellular = False
-            if "셀룰러" in title:
-                cellular = True
-            elif "cellular" in title:
-                cellular = True
-            elif "Cellular" in title:
-                cellular = True
-            if "기가" in title:
-                index = title.find("기가")
-                size = title[(index-3):index]
-            elif "16G" in title:
-                size = "16"
-            elif "32G" in title:
-                size = "32"
-            elif "64G" in title:
-                size = "64"
-            elif "128G" in title:
-                size = "128"
-            elif "256G" in title:
-                size = "256"
-            elif "512G" in title:
-                size = "512"
-
-            price = price[0].text.strip()
-            status = status[0].find("span").text
-            exchange = exchange[0].find("span").text
-            shipping = shipping[0].find("span").text
-            location = location[0].find("span").text.strip()
-            location = location.split("\n")
-            location = location[0]
+        sell = soup.select("#app > div.router-view > div > div.product-detail-wrapper > div:nth-child(2) > div:nth-child(1) > div.suggested-products-title")
+        if len(sell)==0:
+            title = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-jrIrqw.hOgugl > div.sc-hjRWVT.bbGQaP")
+            price = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-jrIrqw.hOgugl > div.sc-iybRtq.eazFIt > div")
+            status = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-cEvuZC.jcUgmG > div.sc-dxZgTM.eVVlYD > div:nth-child(1) > div.sc-iFMziU.eMXTei")
+            exchange = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-cEvuZC.jcUgmG > div.sc-dxZgTM.eVVlYD > div:nth-child(2) > div.sc-iFMziU.eMXTei")
+            location = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-cEvuZC.jcUgmG > div.sc-dxZgTM.eVVlYD > div:nth-child(4) > div.sc-iFMziU.cpTpen")
+            description = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-OxbzP.fGXcye > div.sc-kqlzXE.cVGKCL > div.sc-cPuPxo.gQmCMT > div.sc-dREXXX.bQuoGl > div.sc-hcmgZB.gsXioq > div.sc-dHmInP.hQhrav")
+            check_date = soup.select("#root > div > div > div.sc-koErNt.hoSImv > div.sc-gJqsIT.eMTckw > div > div.sc-cFlXAS.hJvCHs > div > div.sc-jRhVzh.bynJLV > div > div.sc-gPzReC.fWxkCf > div.sc-cEvuZC.jcUgmG > div.sc-kXeGPI.grTZPy > div > div:nth-child(3)")
+            date = ""
+            if len(check_date)!=0:
+                check_date = check_date[0].text
+                if "일 전" in check_date:
+                    for i in check_date:
+                        if i=="일":
+                            break
+                        upload_date += i
+                    upload_date*=1
+                    date = datetime(today.year, today.month, today.day) - timedelta(upload_date)
+                    date = date.strftime("%Y%m%d")
+                else:
+                    date = today
+            id = data
+            title = title[0].text
+            price = price[0].text
+            status = status[0].text
+            exchange = exchange[0].text
+            if len(location)!=0:
+                location = location[0].text
             description = description[0].text
 
-            request_data['bungae'].append({
-                'id' : data,
-                'company' : "애플",
-                'title' : title,
-                'price' : price,
-                'status' : status,
-                'exchange' : exchange,
-                'shipping' : shipping,
-                'region' : location,
-                'cellular' : cellular,
-                'size' : size+"G",
-                'description' : description
-            })
-        #cnt 주석 예정#####################
-        if cnt==1:
-            break
-    else:
-        print("상품이 판매되었습니다.")
+            size = ""
+            if "매입" not in title:
+                cellular = False
+                if "셀룰러" in title:
+                    cellular = True
+                elif "cellular" in title:
+                    cellular = True
+                elif "Cellular" in title:
+                    cellular = True
+                if "기가" in title:
+                    index = title.find("기가")
+                    size = title[(index-3):index]
+                size_list = ["16","32","64","128","256","512"]
+                if len(size) == 0:
+                    for s_l in size_list:
+                        if s_l in title:
+                            size = s_l
+                            break
+                
+                inch = ""
+                generation = ""
+                if "세대" in title:
+                    index1 = title.find("세대")
+                    generation = title[(index1-1):index1]
+                inch_list =['12.9','11','10.2','10.5','9.7','7.9']
+                for i_c in inch_list:
+                    if i_c in title:
+                        inch = i_c
+                        break
+                category_list = ["미니", "프로", "에어"]
+                for c_g in category_list:
+                    if c_g in title:
+                        category = c_g
+                        break
+                print(title)
+                print(price)
+                print(status)
+                print(exchange)
+                print(location)
+                # print(description)
+                print(date)
+                print(generation)
+                print(category)
+                print(inch)
+                print(cellular)
+                print(size)
+                print(url)
+                print(img_url_list[cnt])
+                
+                request_data['bungae'].append({
+                    'id' : data,
+                    'price' : price,
+                    'company' : "애플",
+                    'title' : title,
+                    'generation' : generation,
+                    'category' : category,
+                    'inch' : inch,
+                    'cellular' : cellular,
+                    'size' : size+"G",
+                    'region' : location,
+                    "date" : date,
+                    'link' : url,
+                    'img_url' : img_url_list[cnt],
+                    'status' : status,
+                    'exchange' : exchange,
+                    # 'description' : description
+                })
+                cnt+=1
+            #cnt 주석 예정#####################
+            # if cnt==1:
+            #     break
+            time.sleep(5)
+        else:
+            print("상품이 판매되었습니다.")
+    except:
+        print("제대로 처리 되지 않습니다")
+        continue
+
 print(request_data)
     
