c988aff478cdaefb5cb2cb1fb5fb4d46dd4c8494
june <june3360@gmail.com>
2019-10-21 09:40:39 +0900

    S1P3151006-9 itemDetailpage uiux 수정(item image border 추가 , grid 변경, floating button 틀 구성, 주석 추가 등

--- frontend/src/components/itemDetail/ItemListCard.vue
+++ frontend/src/components/itemDetail/ItemListCard.vue
@@ -1,45 +1,72 @@
+<!-- itemListCard 아이템 출력 -->
 <template>
   <v-hover v-slot:default="{ hover }">
     <v-card :elevation="hover ? 8 : 2">
-        <!-- -------------itemListCardComponent--------------- -->
-        <div text-center>
-              <v-list-item>
-                <v-list-item-content>
-                  <v-row>
-                    <v-col md="4">
-                      <v-dialog v-model="enlargeImg"  max-width="290">
-                      <template v-slot:activator="{ on }">
-                        <v-img dark v-on="on" :src="img" style="width:200px; height:200px"/>
-                      </template>
-                      <v-card><v-img :src="img"/></v-card>
-                      </v-dialog>
-                    </v-col>
-                    <v-col md="6">
-                     
+      <!-- -------------itemListCardComponent--------------- -->
+      <div text-center>
+        <v-list-item>
+          <v-list-item-content>
+            <v-row>
+              
+              <!-- start : itemImage - 아이템 이미지 출력 및 확대-->
+              <v-col md="4" align-self="center">
+                <v-dialog v-model="enlargeImg">
+                <template v-slot:activator="{ on }">
+                    <v-img class="outlineImg" v-on="on" :src="item.img" style="width:200px; height:200px"/>
+                </template>
+                <v-card><v-img :src="item.img"/></v-card>
+                </v-dialog>
+              </v-col>
+              <!-- start : itemImage -->
+              
+              <!-- start : itemInfomation table - 아이템 정보 출력 부분-->
+              <v-col md="6">
+                <v-simple-table>
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th class="text-left">모델명</th>
+                        <th class="text-left">{{item.name}}</th>
+                      </tr>
+                    </thead>
+                    <thead>
+                      <tr>
+                        <th>용량</th>
+                        <th>{{item.gb}}GB</th>
+                      </tr>
+                    </thead>
+                    <thead>
+                      <tr>
+                        <th>등록 날짜</th>
+                        <th>{{item.registrationDate}}</th>
+                      </tr>
+                    </thead>
+                    <thead>
+                      <tr>
+                        <th>판매 지역</th>
+                        <th>{{item.area}}</th>
+                      </tr>
+                    </thead>
+                    <thead>
+                      <tr>
+                        <th>가격</th>
+                        <th style="color:#489489">{{item.price}}</th>
+                      </tr>
+                    </thead>
+                  </template>
+                </v-simple-table>
+              </v-col>
+              <v-col md="2" align-self="center">
+                <v-btn text small color="error" :href="item.link" target="_blank">방문하기</v-btn>                     
+              </v-col>
+              <!-- end : itemInfomation table -->
 
-
-
-                    </v-col>
-                    <v-col md="2">
-                      <v-btn text small color="error" :href="link" target="_blank">방문하기</v-btn>                     
-                    </v-col>
-                  </v-row>
-                </v-list-item-content>
-              </v-list-item>
-        </div>
+            </v-row>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
     </v-card>
   </v-hover>
-
-
-   <!-- <v-row style="height: 50px;">
-                        <v-list-item-title>{{ price }}</v-list-item-title>
-                      </v-row>
-                      <v-row>
-                        <v-list-item-title class="headline">{{ name }}</v-list-item-title>
-                      </v-row>
-                      <v-row>
-                        <p>{{ body }}</p>
-                      </v-row> -->
 </template>
 
 
@@ -51,41 +78,19 @@
 
 export default {
   data: () => ({
-    enlargeImg: false,
-    items: [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%',
-      },
-    ]
+    enlargeImg: false,  // 이미지 확대
   }),
   props: {
-    name: {
-      type: String,
-      default: ""
-    },
-    body: {
-      type: String,
-      default: ""
-    },
-    link: {
-      type: String,
-      default: ""
-    },
-    price: {
-      type: Number,
-      default: 0
-    },
-    img: {
-      type: String,
-      default: ""
+    item: {
+      type: Object,
+      default: () => new Object(),
     }
   },
 };
 </script>
+
+<style>
+.outlineImg { /*이미지 외곽선 css */
+  max-width:95%;border:3px dashed #545565;
+}
+</style>
