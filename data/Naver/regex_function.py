1f9f70e80d21aa6dd5d7c291b35bc95cf0b89b07
신진수 <sjins0127@gmail.com>
2019-10-21 15:47:01 +0900

    navercafe 크롤링 직후 바로 데이터 전처리

--- data/Naver/regex_function.py
+++ data/Naver/regex_function.py
@@ -1,61 +1,293 @@
 #-*- coding:utf-8 -*-
-
-
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 import time
 import urllib.request
 import re
-
-# text = "배터리*아이패드 프로 2세대 10.5인치 256gb (와이파이)팝니다. *아이패드 프로 2세대 10.5인치 256gb (와이파이)팝니다. 구매일은 2018년 8월 30일입니다. 애플 케어 먹여서 20년 8월 30일까지 리퍼 가능하고, 19년 8월 30일 (19년6월 생산)에 리퍼 한번 받아서 상태 S급입니다. 애플 펜슬은 효율 아직 좋습니다.  아이패드, 애플 펜슬 풀박스이고, 충전단자는 제가 따로사서 써서,  내부 USB, 케이블 다 새상품입니다. 리퍼기간 약 10개월 남았고, 내부 액세사리 새상품, 풀박스, +강화필름, 케이스 (펜슬 케이스, 실리콘 케이스, 하드케이스 2개) 구성 상품입니다.  가격은 700,000원 이구요 직거래는 부산대 가능합니다 안전거래도 가능합니다.  많은 연락바랍니다. 010-2사83-25팔4"
-
-# text = "아이패드 미니5 256G 와이파이모델 그레이색상 팝니다 (미개봉) 그레이색상 1대 53만입니다  직거래 성남입니다 010 2300 3999"
-
-# text="아이패드 프로 3세대, 12.9인치, 셀룰러, 512기가 팝니다 (상태 A++) 1,400,000원 밖에 들고나간 적도 한번도 없고 집에서도 거의 사용을 안 하고 모셔만 둔 거라  상태는 그냥 새 것과 같다고 보셔도 좋을 정도로 아주 깨끗합니다.  아이패드는 정상해지 완료해서 통신사 등록 및 확정기변 가능하세요.  리퍼기간은 2020. 7. 29까지입니다.  가격: 140만원 ※ 에눌 문의는 일절 사양합니다!! 문의주셔도 답변하지 않습니다~  거래방법: 직거래만 합니다! (※ 사정상 서울 7호선 장승배기역에서만 거래 가능해요!) 연락처: 010-3삼사8-삼54공 (문자로 주세요~) 구매시 미사용 액정필름도 같이 드립니다.  현재 부착된 필름은 랩씨 종이질감 스케치 필름입니다 제가 붙인거라 안에 있는 먼지를 제거한다고 끝부분을 한번 뗐다가 다시 붙였더니 아래 사진처럼 기포가 ㅠㅠ 사용시 불편한 점은 없는데 거슬리시면 그냥 떼고 쓰시면 될 것 같아요 그 외 따로 말씀드릴 사항 없이 상태 아주 깨끗합니다~ "
-# '원$' #원으로 끝나야한다.
-
-text = "1,000,000원"
-# text = "1,000원"
-# text = "1000원"
-# text = "100만입니다"
-text2 = "100만원입니다"
-text2 = "8.9만"
-#+'\d+' # \d+ : 숫자가 하나 이상이어야 함
-
-# regex = re.compile("\d+만\w*")
-# mc = regex.findall(text)
-# print(mc)
-try:
-    regex = re.compile(r"(\d*?[,]?\d*?[,]?\d+)원\w*")
-    mc = regex.search(text)
-    print(mc.group(1))
-
-    print("=>"+mc.group(1).replace(",", "")+"원")
-except:
-    pass
-
-try:
-    regex = re.compile(r"(\d*?[,|.]?\d*?[,|.]?\d+)만[원]*\w*")
-    mc = regex.search(text2)
-    print(mc.group(1))
-    num = float(mc.group(1).replace(",",""))*10000
-    print(num)
-    print("=>" + str(round(num)) + "원")
-except:
-    pass
-
-# regex = re.compile(r"(\d+)[기가|GB|gb]\w*")
-# mc = regex.search(text)
-# print(mc.group(1))
-
-# regex = re.compile("\d+만\w*")
-# mc = regex.findall(text)
-# print(mc)
+import csv
+import pandas as pd
+from collections import Counter
 
 
-ttt = "date&articleid=653494415&referrerAllArticles=false"
 
-regex = re.compile(r"articleid=(\d+)&")
-mc=regex.search(ttt)
-print(mc.group(1))
+def get_model(request_data,read_title,read_text):
+    model = "" # 아이패드2 아이패드미니 아이패드프로, 아이패드에어
+    gen = "" # ?세대
+    inch = "" #몇인치인가?
+
+#인치를 타이틀에서 삭제
+    inch_list =['12.9','11','10.2','10.5','9.7','7.9']
+    inch = "".join([s for s in inch_list if s in read_title])
+    read_title = read_title.replace(inch,"")
+    # print("/////////인치 제거한 타이틀",read_title)
+
+
+##용량을 타이틀에서 삭제
+    memory = get_memory(read_title,"") #용량을 가져옴
+    if memory:
+        memory_list = ['GB','기가','G']
+        for i in range(len(memory_list)):
+            memory_list[i]=memory+memory_list[i]
+        delete_text = "".join([s for s in memory_list if s in read_title])
+        read_title = read_title.replace(delete_text, "")
+        # print("/////////용량제거한 타이틀",read_title)
+        memory=memory+"GB"
+    else:
+        if "1TB" in read_title:
+            memory = '1TB'
+            read_title = read_title.replace("1TB", "")
+            # print("//////용량제거한 타이틀", read_title)
+
+##큰모델 가져오기
+    if "미니" in read_title:
+        model = "아이패드 미니"
+        try:
+            regex = re.compile(r"(?<=[미니|미니 ])(\d)")
+            # regex = re.compile(r"[미니|미니 ]?(\d)")
+            mc = regex.search(read_title)
+            gen = mc.group(1)
+        except:
+            pass
+    elif "에어" in read_title:
+        model = "아이패드 에어"
+        try:
+            regex = re.compile(r"(?<=[에어|에어 ])(\d)")
+            mc = regex.search(read_title)
+            gen = mc.group(1)
+        except:
+            pass
+    elif "프로" in read_title:
+        model = "아이패드 프로"
+        try:
+            regex = re.compile(r"(?<=[프로|프로 ])(\d)")
+            # regex = re.compile(r"[프로|프로 ]?(\d)")
+            mc = regex.search(read_title)
+            gen = mc.group(1)
+        except:
+            pass
+    elif "PRO" in read_title:
+        model = "아이패드 프로"
+        try:
+            regex = re.compile(r"(?<=[PRO|PRO ])(\d)")
+            # regex = re.compile(r"[PRO|PRO ]?(\d)")
+            mc = regex.search(read_title)
+            gen = mc.group(1)
+        except:
+            pass
+    else:
+        model = "아이패드"
+        try:
+            # regex = re.compile(r"[미니|미니 ]?(\d)")
+            regex = re.compile(r"(?<=[아이패드|아이패드 ])(\d)")
+            mc = regex.search(read_title)
+            gen = mc.group(1)
+        except:
+            pass
+    if gen is "":
+        try:
+            regex = re.compile(r"(\d)세대")
+            mc = regex.search(read_title)
+            gen=mc.group(1)
+        except:
+            pass
+
+
+    cellular=None
+    boolean_cellular = get_cellular(read_title,read_text)
+    if boolean_cellular:
+        cellular = "셀룰러"
+    else:
+        cellular = "WIFI"
+
+    if gen is not "":
+        gen = gen+"세대"
+    print("모델:",model," /세대:", gen," /인치:",inch,"/용량:",memory,"/LTE:",cellular)
+
+    if model is "":
+        pass
+    if gen is "":
+        pass
+    if inch is "":
+        pass
+    if memory is "":
+        pass
+    if cellular is "":
+        pass
+    request_data['model']=model
+    request_data['generation']=gen
+    request_data['display']=inch
+    request_data['memory']=memory
+    request_data['cellular']=cellular
+
+    return request_data
+    # result = " ".join(request_data).replace("  "," ")
+    # print(result)
+    # return model
+
+
+def get_memory(read_title,read_text):
+    memory = ""
+    try:
+        regex = re.compile(r"(\d+)[기가|GB|gb|g]\w*")
+        mc = regex.search(read_title)
+        # print(mc.group(1))
+        memory=mc.group(1)
+    except:
+        pass
+    if memory is "":
+        try:
+            regex = re.compile(r"(\d+)[기가|GB|G]\w*")
+            mc = regex.search(read_text)
+            print(mc.group(1))
+            memory = mc.group(1)
+        except:
+            pass
+
+    if memory is "":
+        memory_list =['16','32','64','128','256','512']
+        memory = "".join([s for s in memory_list if s in read_title])
+
+    return memory
+
+def get_cellular(read_title,read_text):
+
+
+    is_cellular = False
+    is_wifi = False
+    cellurlar_list= ['셀룰러','LTE','셀롤러']
+    if any(format in read_title for format in cellurlar_list):
+        is_cellular=True
+    else:
+        if any(format in read_text for format in cellurlar_list):
+            is_cellular = True
+
+    wifi_list = ['WIFI', '와이파이','WI-FI']
+    if any(format in read_title for format in wifi_list):
+        is_wifi = True
+    else:
+        if any(format in read_text for format in wifi_list):
+            is_wifi = True
+
+    if is_cellular:
+        return True
+    else:
+        return False
+
+def get_price_from_text(price_list,text):
+    try:
+        regex = re.compile(r"(\d*?,?\d*?,?[^.]\d+)원\w*")
+        mc = regex.findall(text)
+        for m in mc:
+            num = int(m.replace(",", ""))
+            if num<=10000:
+                continue
+            price_list.append(int(num))
+            # print(1,num)
+    except:
+        pass
+    # try:
+    #     regex = re.compile(r"(\d*?,?\d*?,?[^.]\d+)[\s|\S]*?만[원]?\w*")
+    #     mc = regex.findall(text)
+    #     for m in mc:
+    #         num = int(m.replace(",", ""))*10000
+    #         price_list.append(num)
+    #         print(2, num)
+    # except:
+    #     pass
+    try:
+        regex = re.compile("(\d+)(?=만)")
+        mc = regex.findall(text)
+        for m in mc:
+            num = int(m.replace(",", "")) * 10000
+            if num<=10000:
+                continue
+            price_list.append(num)
+            # print(3, num)
+    except:
+        pass
+    try:
+        regex = re.compile(r"(\d+[.]\d)만[원]?\w*")
+        mc = regex.findall(text)
+        for m in mc:
+            num = int(m.replace(".", ""))*1000
+            if num<=10000:
+                continue
+            price_list.append(num)
+            # print(4, num)
+    except:
+        pass
+    try:
+        regex = re.compile(r"(\d+.000)[원]?\w*")
+        mc = regex.findall(text)
+        for m in mc:
+            num = int(m.replace(".", ""))
+            if num<=10000:
+                continue
+                # print(5,num)
+            price_list.append(num)
+    except:
+        pass
+
+    try:
+        # regex = re.compile("(\d+)\s(?=만)")
+        regex = re.compile("(\d+)[' '](?=만)")
+        mc = regex.findall(text)
+        for m in mc:
+            num = int(m)*10000
+            price_list.append(num)
+    except:
+        pass
+    return price_list
+
+
+
+def get_price(request_data,read_title,read_price,read_text):
+    price_list=[]
+    get_price_from_text(price_list,read_title)
+    get_price_from_text(price_list,read_price)
+    get_price_from_text(price_list,read_text)
+    # print(price_list)
+    ## 여기서 1순위는 가장 많은것, 2순위 제일 비싼것
+    count=Counter(price_list).most_common(2)
+    # sorted_C_union = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+    # print(sorted_C_union)
+    count = sorted(count,key=lambda x:(-x[1],-x[0]))
+    # print(count)
+    # print(count[0][0])
+
+    most_price = count[0][0]
+
+    in_price=0
+    try:
+        regex = re.compile(r"(\d*?,?\d*?,?[^.]\d+)원\w*")
+        mc = regex.findall(read_price)
+        for m in mc:
+            num = m.replace(",", "")
+            if num<=10000:
+                continue
+            price_list.append(int(num))
+            # print(1,num)
+    except:
+        pass
+    # print("가격",most_price if most_price>=in_price else in_price)
+    request_data["price"]=most_price if most_price>=in_price else in_price
+    return request_data
+
+# if __name__ == '__main__':
+#     with open('navercafe_crawling.csv','r', encoding='utf-8-sig') as read_data:
+#         read_data = list(read_data)
+#         for i in range(len(read_data)) :
+#         # for i in range(5) :
+#             print("#############################################")
+#             read_line = read_data[i].split(',')
+#             read_title = read_line[2].upper()
+#             read_price = read_line[3]
+#             read_text = read_line[4].upper()
+#             print("title",read_title)
+#             print("price",read_price)
+#             print("text",read_text)
+#             request_data={} #딕셔너리 형식
+#             request_data=get_model(request_data,read_title,read_text)
+#             request_data =get_price(request_data,read_title,read_price,read_text)
+#             print(request_data)
--- data/Naver/test.py
+++ data/Naver/test.py
@@ -6,6 +6,8 @@ import urllib.request
 import re
 import csv
 import pandas as pd
+import regex_function
+import json
 
 filename = 'navercafe_passtext.txt'
 passtext = []
@@ -22,7 +24,7 @@ with open(filename, mode='r', encoding='utf-8') as data:
 chrome_options= webdriver.ChromeOptions() #옵션 설정하기
 # chrome_options.add_argument('headless') #창이 안보이도록 숨기기
 chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.3')
-driver = webdriver.Chrome("./chromedriver.exe",chrome_options=chrome_options)
+driver = webdriver.Chrome("../chromedriver.exe",chrome_options=chrome_options)
 driver.implicitly_wait(3)
 # driver.implicitly_wait(3)
 driver.get('https://nid.naver.com/nidlogin.login')
@@ -53,7 +55,7 @@ except:
 
 page_number = 1
 for page_num in range(page_number):
-    search_url="https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10050146&search.menuid=749&search.media=0&search.searchdate=all&search.exact=&search.include=&userDisplay=50&search.exclude=&search.onSale=1&search.option=3&search.sortBy=date&search.searchBy=0&search.searchBlockYn=0&search.includeAll=&search.query=%BE%C6%C0%CC%C6%D0%B5%E5&search.viewtype=title&search.page="+str(page_num+1)
+    search_url="https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10050146&search.menuid=749&search.media=0&search.searchdate=all&search.exact=&search.include=&userDisplay=5&search.exclude=&search.onSale=1&search.option=3&search.sortBy=date&search.searchBy=0&search.searchBlockYn=0&search.includeAll=&search.query=%BE%C6%C0%CC%C6%D0%B5%E5&search.viewtype=title&search.page="+str(page_num+1)
     # search_url="https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10050146&search.menuid=749&search.media=0&search.searchdate=all&search.defaultValue=1&search.exact=&search.include=&userDisplay=50&search.exclude=&search.onSale=1&search.option=3&search.sortBy=date&search.searchBy=0&search.searchBlockYn=0&search.includeAll=&search.query=%BE%C6%C0%CC%C6%D0%B5%E5&search.viewtype=title&search.page="+str(page_num)
     driver.get(search_url)
     driver.implicitly_wait(3)
@@ -104,17 +106,19 @@ for page_num in range(page_number):
             tt = page.find(id="tbody")
             img_src = tt.find(class_="image_condition").find("img").get('src')
             date = page.find(class_="tit-box").find(class_="date").text
-
             regex = re.compile(r"articleid=(\d+)&")
             mc = regex.search(info_url)
             # print(mc.group(1))
-            result.append(mc.group(1)) # 아이디
+            # result.append(mc.group(1)) # 아이디
+
+            id=mc.group(1)#아이디
             simple_url = "https://cafe.naver.com/joonggonara/"+mc.group(1)
-            result.append(simple_url) #url
+            # result.append(simple_url) #url
 
-
-            result.append(tt.find(class_="title").text) #타이틀
-            result.append(tt.find(class_="cost").text) #가격
+            read_title = tt.find(class_="title").text
+            read_price = tt.find(class_="cost").text
+            # result.append(tt.find(class_="title").text) #타이틀
+            # result.append(tt.find(class_="cost").text) #가격
 
             test = tt.find_all(string=True)
             index = None
@@ -128,17 +132,46 @@ for page_num in range(page_number):
             # print(" ".join(test).strip())
             text.append(" ".join(test).strip())
             # text.append('\n')
-            data = " ".join(text)
-            result.append(data) #텍스트
-            result.append(date) #날짜
-            result.append(img_src) #이미지 주소
+
+            read_text = " ".join(text)#텍스트
+
+            result.append(read_title)
+            result.append(read_price)
+            result.append(read_text)
+            result.append(id)
+            result.append(date)#날짜
+            result.append(img_src)#이미지 주소
+            result.append(simple_url)
+
+
+
+
+
+
+
             save_result.append(result)
             # f.write(data)
         except:
+            print("에러에러에ㅓ에러에]")
             continue
-
-    df = pd.DataFrame.from_records(save_result, columns=labels)
-    df = df.applymap(lambda x: x.replace('\xa0','').replace('\xa9','').replace(',',''))
-    df.to_csv("navercafe_crawling.csv",encoding="utf-8-sig",header=False,index=False,mode='a')
+    # df = pd.DataFrame.from_records(save_result, columns=labels)
+    # df = df.applymap(lambda x: x.replace('\xa0','').replace('\xa9','').replace(',',''))
+    # df.to_csv("navercafe_crawling.csv",encoding="utf-8-sig",header=False,index=False,mode='a')
 
     # f.close()
+
+    # for index in range(1):
+    for li in save_result:
+        print("##############################################################################################################")
+        # li = save_result[index]
+        # print(li)
+        read_title=li[0]
+        read_price=li[1]
+        read_text=li[2]
+        print(read_title)
+        print(read_price)
+        print(read_text)
+        product={}
+        product=regex_function.get_model(product,read_title,read_text)
+        product =regex_function.get_price(product,read_title,read_price,read_text)
+        print("도오잉",product)
