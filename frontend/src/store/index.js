3d8d383010a87c2117a19fa82cb9fc88bb985d45
이중봉 <wndqhd8@naver.com>
2019-10-23 15:51:06 +0900

    S1P3151006-17 | vuex에 데이터 저장중

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import data from './modules/data'
+import searchData from './modules/searchData'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     data,
+    searchData,
   },
 })
---
+++ frontend/src/store/modules/searchData.js
@@ -0,0 +1,23 @@
+
+
+const state = {
+    selected : [],
+  }
+  const getters = {
+    getSelected(state) {
+      return state.selected;
+    }
+  }
+  const mutations = {
+  }
+  const actions = {
+  
+  }
+  export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+  }
+  
