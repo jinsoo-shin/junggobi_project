1d3088629aa756bd5f5e1b69a2c5193e8d06ca0c
이중봉 <wndqhd8@naver.com>
2019-10-23 14:00:52 +0900

    S1P3151006-17 | 검색기능구현중

--- frontend/src/components/home/CheckBoxButton.vue
+++ frontend/src/components/home/CheckBoxButton.vue
@@ -1,11 +1,18 @@
 <template>
     <div id="app">
-        <input type="checkbox" v-model ="objectData" true-value='yes' false-value='no'>
+        <!-- <input type="checkbox" v-model ="objectData" true-value='yes' false-value='no'>
         <label for="jack">{{this.objectData}}}</label>
         <input type="checkbox" true-value='yes' false-value='no' v-model="objectKey">
         <label for="john">{{this.objectKey}}</label>
         <input type="checkbox" true-value='yes' false-value='no' v-model="objectValue">
-        <label for="mike"><span>{{this.objectValue}}</span></label>
+        <label for="mike"><span>{{this.objectValue}}</span></label> -->
+        <li v-for="(eachValue,index) in objectValue" :key=index>
+            <button v-on:click="clickevent(eachValue)">
+            <input type="checkbox" id="$`eachValue`" value="$`eachValue`" >
+            </button>
+            <label for="$`eachValue`">{{eachValue}}</label>  
+        </li>
+
         <br>
         <span>체크한 이름: </span>
     </div>
@@ -14,6 +21,7 @@
 export default {
     data() {
         return{
+            selected:[],
             
         }
     },
@@ -21,7 +29,18 @@ export default {
     objectData:{type:Object},
     objectKey:{type:String},
     objectValue:{type:Array},
-},
+    },
+    methods:{
+        clickevent(da) {
+            let idx = this.selected.indexOf(da)
+            if (idx > -1) {
+                this.selected.splice(idx,1)
+            } else {
+                this.selected.push(da)
+        }
+        console.log(this.selected)
+        }
+    }
 }
 </script>
 <style>
