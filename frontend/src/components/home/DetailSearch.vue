812324ea7156a2bbaa2adb3c8739309294958afa
조신성 <nustar1005@naver.com>
2019-10-30 17:11:59 +0900

    S1P3151006-69 | 메인페이지 전체 디자인 끝

--- frontend/src/components/home/DetailSearch.vue
+++ frontend/src/components/home/DetailSearch.vue
@@ -1,118 +1,92 @@
 <template>
   <v-flex id="back">
-    <div id="font1">검색 옵션</div>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-combobox
-          :items="items"
-          v-model="value"
-          label="어떤 기기를 원하시나요?"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value=='태블릿'">
+    <v-sheet
+      elevation="12"
+      class="pa-12"
+    >
+      <div id="font1">검색 옵션</div>
+      <v-row>
+        <v-col cols="12" sm="6">
           <v-combobox
-            :items="tablet"
-            v-model="value1"
-            
-            label="태블릿 종류를 고르세요"
+            :items="items"
+            v-model="value"
+            label="어떤 기기를 원하시나요?"
           ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value1=='아이패드'">
-        <v-combobox
-          :items="ipad_category"
-          v-model="value3"
-          
-          label="아이패드 종류"
-        ></v-combobox>
-        <v-combobox
-          :items="ipad_gene"
-          v-model="value4"
-          
-          label="아이패드 세대"
-        ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value=='태블릿'">
+            <v-combobox
+              :items="tablet"
+              v-model="value1"
+              
+              label="태블릿 종류를 고르세요"
+            ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value1=='아이패드'">
           <v-combobox
-          :items="ipad_wifi"
-          v-model="value5"
-          
-          label="셀룰러/WIFI"
-        ></v-combobox>
+            :items="ipad_category"
+            v-model="value3"
+            label="아이패드 종류"
+          ></v-combobox>
           <v-combobox
-          :items="ipad_size"
-          v-model="value2"
-          
-          label="태블릿 크기"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value1=='갤럭시탭'">
-        <v-combobox
-          :items="galaxy_category"
-          v-model="value3"
-          
-          label="갤럭시탭 종류"
-        ></v-combobox>
+            :items="ipad_gene"
+            v-model="value4"
+            label="아이패드 세대"
+          ></v-combobox>
+            <v-combobox
+            :items="ipad_wifi"
+            v-model="value5"
+            label="셀룰러/WIFI"
+          ></v-combobox>
+            <v-combobox
+            :items="ipad_size"
+            v-model="value2"
+            label="태블릿 크기"
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value1=='갤럭시탭'">
           <v-combobox
-          :items="ipad_wifi"
-          v-model="value5"
-          
-          label="셀룰러/WIFI"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value=='모바일/스마트폰'">
-        <v-combobox
-          v-model="value6"
-          :items="mobile"
-          
-          label="제조사를 골라주세요"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value6=='삼성전자 갤럭시'">
-        <v-combobox
-          v-model="value7"
-          :items="gal_category"
-          
-          label="갤럭시"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value7=='갤럭시 S'">
-        <v-combobox 
-          v-model="value8"
-          :items="s_category"
-          chips
-          label="갤럭시 S라인"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value7=='갤럭시 노트'">
-        <v-combobox
-          v-model="value9"
-          :items="note_category"
-          
-          label="갤럭시 노트라인"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value7=='그 외'">
-        <v-combobox
-          v-model="value10"
-          :items="g_category"
-          
-          label="갤럭시 노트라인"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="12" sm="6" v-if="value6=='애플 아이폰'">
-        <v-combobox
-          v-model="value11"
-          :items="iphone_category"
-          
-          label="아이폰 세대"
-        ></v-combobox>
-      </v-col>      
-      <v-col cols="12" sm="6" >
-        <center>
+            :items="galaxy_category"
+            v-model="value3"
+            
+            label="갤럭시탭 종류"
+          ></v-combobox>
+            <v-combobox
+            :items="ipad_wifi"
+            v-model="value5"
+            
+            label="셀룰러/WIFI"
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value=='모바일/스마트폰'">
+          <v-combobox
+            v-model="value6"
+            :items="mobile"
+            
+            label="제조사를 골라주세요"
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value6=='삼성전자 갤럭시'">
+          <v-combobox
+            v-model="value7"
+            :items="s_category"
+            label="갤럭시"
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6" v-if="value6=='애플 아이폰'">
+          <v-combobox
+            v-model="value11"
+            :items="iphone_category"
+            
+            label="아이폰 세대"
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" sm="6">
           <v-btn x-large color="blue lighten-2" dark id="font">
             <v-icon dark left>mdi-checkbox-marked-circle</v-icon> 검색
           </v-btn>
-        </center>
-      </v-col>
-    </v-row>
+        </v-col>      
+      </v-row>
+    </v-sheet>
   </v-flex>
 </template>
 
@@ -138,14 +112,8 @@ export default {
       iphone_category: ['상관없음', '11', '11 프로', 'XS', 'XS 플러스', 'XS MAX', 'XR', 'X',
       'S', '8 플러스', '8', '7 플러스', '7', '6S', '6 플러스', '6'],
       value7 : ['갤럭시 종류'],
-      gal_category: ['갤럭시 S', '갤럭시 노트', '그 외', '상관없음'],
-      value8 : ['갤럭시 S라인'],
       s_category : ['S10','S10플러스','S10E', 'S9', 'S9플러스', 'S8', 'S8플러스',
-      'S7', 'S7엣지', 'S6', 'S5', 'S3'],
-      value9 : ['갤럭시 노트라인'],
-      note_category : ['10', '10플러스', '9', 'FE', '8', '8플러스', '7', '5', '4'],
-      value10 : ['전체 갤럭시'],
-      g_category : ['A30', 'A5', 'A7', 'A8', 'A9', 'J6'],
+      'S7', 'S7엣지', 'S6', 'S5', 'S3', '노트10', '노트10플러스', '노트9', '노트FE', '노트8', '노트8플러스', '노트7', '노트5', '노트4', 'A30', 'A5', 'A7', 'A8', 'A9', 'J6'],
       value11 : ['아이폰 세대']
       
     }
@@ -160,6 +128,6 @@ export default {
 }
 #font1 {
   font-family: 'Cute Font', cursive;
-  font-size: 60px;
+  font-size: 40px;
 }
 </style>
--- frontend/src/components/home/Search.vue
+++ frontend/src/components/home/Search.vue
@@ -1,22 +1,12 @@
 <template>
     <v-flex>
         <v-text-field
-            v-model="value"
-            color="rgb(27, 99, 158)"
-            label="필요한 걸 찾아요!"
-            placeholder="아이패드, 아이폰, 갤럭시S, 갤럭시탭 등을 검색하세요."
-            loading
-        >
-            <template v-slot:progress>
-                <v-progress-linear
-                    v-if="custom"
-                    :value="progress"
-                    color="rgb(96, 172, 235)"
-                    absolute
-                    height="7"
-                ></v-progress-linear>
-            </template>
-        </v-text-field>
+            v-model="model"
+            :outlined="outlined"
+            :rounded="rounded"
+            :solo="solo"
+            :clearable="clearable"
+        ></v-text-field>
     </v-flex>
 </template>
 
@@ -24,13 +14,12 @@
 export default {
     data: () => ({
         value: '',
-        custom: true,
+        model: '',
+        outlined: true,
+        rounded: true,
+        solo: true,
+        clearable: true
     }),
-    computed: {
-        progress () {
-            return Math.min(100, this.value.length * 10)
-        }
-    }
 }
 </script>
 
