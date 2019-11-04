c59dbc44005d646362882c966aacd9cc3d8de241
root <root@ip-172-26-12-98.ap-northeast-2.compute.internal>
2019-11-04 02:21:49 +0000

    S1P3151006-79 | 충돌해결2

---
+++ frontend/src/components/oldFile/ItemDetailPage.vue
@@ -0,0 +1,68 @@
+<!-- itemDetailPage 상세 페이지 -->
+<template>
+    <v-container grid-list-md text-center>  
+        <v-flex>
+            <v-text-field
+                v-model="value"
+                :outlined="outlined"
+                :rounded="rounded"
+                :solo="solo"
+                :clearable="clearable"
+                v-on:keyup.enter="searchById(value)"
+            ></v-text-field>
+        </v-flex> 
+        <v-fab-transition>
+            <v-btn
+                @click="side"
+                color="grey"
+                fab
+                dark
+                small
+                absolute
+                right
+            >
+                <span class="mdi mdi-playlist-plus mdi-24px"></span>
+            </v-btn>
+        </v-fab-transition>
+        <!-- start : item-list -->
+        <itemList :itemListCards="itemList"/>
+        <!-- end : item-list -->
+        
+        <!-- start : sideMenu -->
+        <sideMenu></sideMenu>
+        <!-- end : sideMenu -->
+    
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "itemDetailPage",
+    data: () => ({
+        value: '',
+        outlined: true,
+        rounded: true,
+        solo: true,
+        clearable: true,
+        itemList: [],
+    }),
+    methods: {
+        side() { 
+            this.EventBus.$emit("sideMenu", true)
+        },
+        async searchById(id) {
+            await this.$store.dispatch('data/searchById', id)
+            .then(res => {
+                console.log(res)
+                // let valueMaxMin = this.checkMaxMinValue(res);
+                // this.$store.commit('data/setValueMaxMin', valueMaxMin)
+                this.EventBus.$emit("changedItemList");
+                this.itemList = res;
+            })
+        }
+    },
+    created() {
+        // this.EventBus.$on("changedItemList",() => console.log("changed!"))
+    }
+};
+</script>
